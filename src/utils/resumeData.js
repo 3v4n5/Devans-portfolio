616d9b874c53a6635fb30164ee46e911cb4986ad
Evans mondragon <evananyta@hotmail.com>
2021-04-03 02:02:39 -0500

    perfil, sobre mi, portafolio

--- src/utils/resumeData.js
+++ src/utils/resumeData.js
@@ -2,9 +2,11 @@ import React from 'react'
 import FacebookIcon from '@material-ui/icons/Facebook';
 import TwitterIcon from '@material-ui/icons/Twitter';
 import GitHubIcon from '@material-ui/icons/GitHub';
-import { AssignmentIndOutlined, WebOutlined } from '@material-ui/icons';
+import { AssignmentIndOutlined, GitHub, Language, WebOutlined, YouTube } from '@material-ui/icons';
 import VideogameAssetIcon from '@material-ui/icons/VideogameAsset';
 import CodeIcon from '@material-ui/icons/Code';
+
+
 
 export default {
     name: 'Evans R. Mondragon',
@@ -90,6 +92,65 @@ export default {
             title: 'Web Dev',
             description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
             icon: <CodeIcon />
+        }
+    ],
+
+    projects: [
+        {
+            tag: 'React',
+            image: 'https://images.unsplash.com/photo-1498050108023-c5249f4df085?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1652&q=80',
+            title: 'React Project 1',
+            caption: 'A short description',
+            description: 'Lorem, ipsum dolor sit amet consectetur adipisicing.',
+            links: [
+                [
+                   { link: 'https://www.youtube.com', icon: <YouTube />},
+                   { link: 'https://www.youtube.com', icon: <GitHub />},
+                   { link: 'https://www.youtube.com', icon: <Language />}
+                ]
+            ]
+        },
+        {
+            tag: 'React',
+            image: 'https://images.unsplash.com/photo-1498050108023-c5249f4df085?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1652&q=80',
+            title: 'React Project 2',
+            caption: 'A short description',
+            description: 'Lorem, ipsum dolor sit amet consectetur adipisicing.',
+            links: [
+                [
+                   { link: 'https://www.youtube.com', icon: <YouTube />},
+                   { link: 'https://www.youtube.com', icon: <GitHub />},
+                   { link: 'https://www.youtube.com', icon: <Language />}
+                ]
+            ]
+        },
+        {
+            tag: 'Python',
+            image: 'https://images.unsplash.com/photo-1498050108023-c5249f4df085?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1652&q=80',
+            title: 'Python Project 1',
+            caption: 'A short description',
+            description: 'Lorem, ipsum dolor sit amet consectetur adipisicing.',
+            links: [
+                [
+                   { link: 'https://www.youtube.com', icon: <YouTube />},
+                   { link: 'https://www.youtube.com', icon: <GitHub />},
+                   { link: 'https://www.youtube.com', icon: <Language />}
+                ]
+            ]
+        },
+        {
+            tag: 'Java',
+            image: 'https://images.unsplash.com/photo-1498050108023-c5249f4df085?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1652&q=80',
+            title: 'Java Project 1',
+            caption: 'A short description',
+            description: 'Lorem, ipsum dolor sit amet consectetur adipisicing.',
+            links: [
+                [
+                   { link: 'https://www.youtube.com', icon: <YouTube />},
+                   { link: 'https://www.youtube.com', icon: <GitHub />},
+                   { link: 'https://www.youtube.com', icon: <Language />}
+                ]
+            ]
         }
     ]
     
